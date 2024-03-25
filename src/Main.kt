5072f9d186dd3bc36cbbc8020f728f0388cb3c28
kimura-khamali <kimurakhamali@gmail.com>
2024-03-25 09:15:37 +0300

    calculating

---
+++ src/Main.kt
@@ -0,0 +1,251 @@
+import java.awt.geom.Area
+
+/*fun main() {
+   val firstName ="Jack"
+   val lastName = " Block"
+   val fullName = firstName +" " + lastName
+   println(fullName)
+}*/
+/*fun main(){
+   var num1 = 45
+   var num2 = 78
+   var result = num1 + num2
+   println(result)
+}*/
+/*fun main(){
+   var num1 = 108
+   var num2 = 40
+   var result = num1 - num2
+   println(result)
+   }*/
+
+/*fun main() {
+   var num1 = 108
+   var num2 = 40
+   var result = num1 * num2
+   println(result)
+}*/
+/*fun main() {
+var num1 = 78
+var num2 = 45.0
+var result = num1 / num2
+println(result)
+}*/
+
+/*fun main() {
+   var num1 = 45
+   var num2 = 78
+   var result = num1 % num2
+   println(result)
+}*/
+
+fun main() {
+   var a = 2
+   var b = 3
+//  a += b
+  a = a + b
+   println(a)
+}
+
+/*fun main() {
+   var a = 2
+   var b = 3
+   a -= b
+   println(a)
+}*/
+
+/*fun main() {
+   var a =3
+   var x = a++
+   println(a)
+   print(x)
+}*/
+
+/*fun main() {
+   var a = 3
+   var x = a--
+   println(x)
+   print(a)
+}*/
+
+/*fun main() {
+   var a = 3
+   var b = 4
+   //println(a > b)
+   //println(a<b)
+     println(a<=b)
+}*/
+
+/*fun main() {
+   var a = 3
+   var b = 4
+   print(a == b)
+}*/
+
+/*fun main() {
+   var a = 3
+   var b = 4
+   print(a != b)
+}*/
+/*fun main(){
+   printMyName()
+}
+fun printMyName(){
+   println("Brenda Khamali")
+}*/
+
+/*fun main(){
+   modulus()
+}
+fun printMyName(){
+   println("Adalab 2024")
+}
+fun modulus(){
+  var num1 = 80
+   var num2 =38
+   var results= num1 % num2
+   println(results)
+}*/
+
+/*fun main(){
+    modulus(num1 =80 , num2 = 38)
+    countCharacter()
+
+    addtwoNumbers(num1=60, num2=51)
+}
+
+fun printMyName(){
+    println("Adalab 2024")
+}
+fun modulus(num1: Int, num2: Int){
+    var results= num1 % num2
+    println(results)
+}
+
+fun countCharacter(){
+    var word = "Avacado"
+    var wordLen = word.length
+    println(wordLen)
+}
+
+fun addtwoNumbers(num1: Int, num2: Int){
+    var sum =num1 + num2
+    println(sum)
+}*/
+
+/*fun main(){
+   printMyName(name: "Brenda")
+}
+fun printMyName(name: String){
+   println("name")
+}
+*/
+/*fun main(){
+   modulus()
+}
+fun printMyName(){
+   println("Adalab 2024")
+}
+fun modulus(){
+  var num1 = 80
+   var num2 =38
+   var results= num1 % num2
+   println(results)
+}*/
+
+
+/*fun main(){
+    modulus(20,
+                 30)
+
+}
+
+fun printMyName(){
+    println("Adalab 2024")
+}
+fun modulus(num1: Int, num2: Int){
+    var results= num1 % num2
+    println(results)
+}*/
+
+/*fun main(){
+
+    countCharacter(word = "Lake Victoria")
+
+    addtwoNumbers(num1 = 39, num2 = 56)
+}
+
+fun countCharacter(word: String ){
+    println(word)
+
+}
+
+fun addtwoNumbers(num1: Int, num2: Int){
+    var sum =num1 + num2
+    println(sum)
+}
+*/
+
+/*The area of a circle is given by  Area = \pi * radius^2*/
+//write a function that takes in
+//thediameter of a circle and print out its area
+
+/*fun main () {
+    areaOfCircle(radius = 22/7 , )
+}
+
+fun areaOfCircle(radius: Int ) {
+    var area = 3.142 * (radius * radius)
+    println(area)
+
+
+}*/
+
+
+/*fun main(){
+    modulus(num1 =80 , num2 = 38)
+    countCharacter()
+
+    addtwoNumbers(num1=60, num2=51)
+}
+
+fun printMyName(){
+    println("Adalab 2024")
+}
+fun modulus(num1: Int, num2: Int){
+    var results= num1 % num2
+    println(results)
+}
+
+fun countCharacter(){
+    var word = "Avacado"
+    var wordLen = word.length
+    println(wordLen)
+}
+
+fun addtwoNumbers(num1: Int, num2: Int){
+    var sum =num1 + num2
+    println(sum)
+}*/
+
+/*fun sumOfWeight(weights: Array<Double>: Array<Double>): Double {
+    return weights.sum()
+}
+fun averageweight(weights: Array<Double>): Double: {
+    var sum= sumOfWeight(weights)
+    var avg=sum/33
+    return avg
+}*/
+
+//write a function that takes in 2 integers and returns
+//the square of their sum
+
+/*fun sumOfInterger(num1: Int , num2: Int): Int{
+    //var num1: Int = 4
+    //var num2: Int = 8
+    var sum = num1 + num2
+    var square = sum * sum
+    return square
+}*/
+
+
